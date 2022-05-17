fdc84d07b54bc204c6bbcdb91479a8c15ac872c0
DaniDevelopment98 <105693050+DaniDevelopment98@users.noreply.github.com>
2022-05-16 21:57:38 -0600

    Add files via upload

---
+++ ExamenAdmision/Form1.Designer.cs
@@ -0,0 +1,364 @@
+﻿
+namespace ExamenAdmision
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNombreLibro = new System.Windows.Forms.TextBox();
+            this.txtAutor = new System.Windows.Forms.TextBox();
+            this.txtCantidadHojas = new System.Windows.Forms.TextBox();
+            this.txtPrecioRenta = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbxClasificacion = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.DTPFechaLibro = new System.Windows.Forms.DateTimePicker();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dgvLista = new System.Windows.Forms.DataGridView();
+            this.NombreLibro = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Autor = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Clasificacion = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Cantidad = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Fecha = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Precio = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.DTPFechaProgramada = new System.Windows.Forms.DateTimePicker();
+            this.label9 = new System.Windows.Forms.Label();
+            this.DTPEntregaReal = new System.Windows.Forms.DateTimePicker();
+            this.lblMulta = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLista)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtNombreLibro
+            // 
+            this.txtNombreLibro.Location = new System.Drawing.Point(13, 40);
+            this.txtNombreLibro.Name = "txtNombreLibro";
+            this.txtNombreLibro.Size = new System.Drawing.Size(127, 20);
+            this.txtNombreLibro.TabIndex = 0;
+            // 
+            // txtAutor
+            // 
+            this.txtAutor.Location = new System.Drawing.Point(194, 40);
+            this.txtAutor.Name = "txtAutor";
+            this.txtAutor.Size = new System.Drawing.Size(116, 20);
+            this.txtAutor.TabIndex = 1;
+            // 
+            // txtCantidadHojas
+            // 
+            this.txtCantidadHojas.Location = new System.Drawing.Point(194, 99);
+            this.txtCantidadHojas.Name = "txtCantidadHojas";
+            this.txtCantidadHojas.Size = new System.Drawing.Size(99, 20);
+            this.txtCantidadHojas.TabIndex = 4;
+            // 
+            // txtPrecioRenta
+            // 
+            this.txtPrecioRenta.Location = new System.Drawing.Point(217, 175);
+            this.txtPrecioRenta.Name = "txtPrecioRenta";
+            this.txtPrecioRenta.Size = new System.Drawing.Size(99, 20);
+            this.txtPrecioRenta.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Nombre del Libro";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(245, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Autor";
+            // 
+            // cmbxClasificacion
+            // 
+            this.cmbxClasificacion.FormattingEnabled = true;
+            this.cmbxClasificacion.Items.AddRange(new object[] {
+            "Terror",
+            "Comedia",
+            "Policiaco",
+            "Ciencias",
+            "Drama"});
+            this.cmbxClasificacion.Location = new System.Drawing.Point(13, 98);
+            this.cmbxClasificacion.Name = "cmbxClasificacion";
+            this.cmbxClasificacion.Size = new System.Drawing.Size(97, 21);
+            this.cmbxClasificacion.TabIndex = 2;
+            this.cmbxClasificacion.Text = "Comedia";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(20, 76);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Clasificacion";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(191, 76);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(94, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Cantidad de Hojas";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(49, 145);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(103, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Fecha de Aquisicion";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(226, 150);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(84, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Precio de Renta";
+            // 
+            // DTPFechaLibro
+            // 
+            this.DTPFechaLibro.Location = new System.Drawing.Point(11, 175);
+            this.DTPFechaLibro.Name = "DTPFechaLibro";
+            this.DTPFechaLibro.Size = new System.Drawing.Size(200, 20);
+            this.DTPFechaLibro.TabIndex = 5;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(260, 212);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.TabIndex = 14;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnAgregar);
+            this.groupBox1.Controls.Add(this.txtAutor);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.cmbxClasificacion);
+            this.groupBox1.Controls.Add(this.DTPFechaLibro);
+            this.groupBox1.Controls.Add(this.txtCantidadHojas);
+            this.groupBox1.Controls.Add(this.txtNombreLibro);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtPrecioRenta);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Location = new System.Drawing.Point(29, 22);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(366, 258);
+            this.groupBox1.TabIndex = 16;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Agregar Libros";
+            // 
+            // dgvLista
+            // 
+            this.dgvLista.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvLista.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.NombreLibro,
+            this.Autor,
+            this.Clasificacion,
+            this.Cantidad,
+            this.Fecha,
+            this.Precio});
+            this.dgvLista.Location = new System.Drawing.Point(26, 286);
+            this.dgvLista.Name = "dgvLista";
+            this.dgvLista.Size = new System.Drawing.Size(646, 150);
+            this.dgvLista.TabIndex = 17;
+            // 
+            // NombreLibro
+            // 
+            this.NombreLibro.HeaderText = "Nombre del Libro";
+            this.NombreLibro.Name = "NombreLibro";
+            // 
+            // Autor
+            // 
+            this.Autor.HeaderText = "Autor";
+            this.Autor.Name = "Autor";
+            // 
+            // Clasificacion
+            // 
+            this.Clasificacion.HeaderText = "Clasificacion";
+            this.Clasificacion.Name = "Clasificacion";
+            // 
+            // Cantidad
+            // 
+            this.Cantidad.HeaderText = "Cantidad de Hojas";
+            this.Cantidad.Name = "Cantidad";
+            // 
+            // Fecha
+            // 
+            this.Fecha.HeaderText = "Fecha de adquisicion";
+            this.Fecha.Name = "Fecha";
+            // 
+            // Precio
+            // 
+            this.Precio.HeaderText = "Precio Renta";
+            this.Precio.Name = "Precio";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnCalcular);
+            this.groupBox2.Controls.Add(this.lblMulta);
+            this.groupBox2.Controls.Add(this.DTPEntregaReal);
+            this.groupBox2.Controls.Add(this.label9);
+            this.groupBox2.Controls.Add(this.DTPFechaProgramada);
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Location = new System.Drawing.Point(448, 22);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(258, 195);
+            this.groupBox2.TabIndex = 20;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Cuentas ";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(39, 40);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(161, 13);
+            this.label8.TabIndex = 0;
+            this.label8.Text = "Fecha Programada Para entrega";
+            // 
+            // DTPFechaProgramada
+            // 
+            this.DTPFechaProgramada.Location = new System.Drawing.Point(20, 58);
+            this.DTPFechaProgramada.Name = "DTPFechaProgramada";
+            this.DTPFechaProgramada.Size = new System.Drawing.Size(200, 20);
+            this.DTPFechaProgramada.TabIndex = 1;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(39, 98);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(117, 13);
+            this.label9.TabIndex = 2;
+            this.label9.Text = "Fecha Real de Entrega";
+            // 
+            // DTPEntregaReal
+            // 
+            this.DTPEntregaReal.Location = new System.Drawing.Point(20, 114);
+            this.DTPEntregaReal.Name = "DTPEntregaReal";
+            this.DTPEntregaReal.Size = new System.Drawing.Size(200, 20);
+            this.DTPEntregaReal.TabIndex = 3;
+            // 
+            // lblMulta
+            // 
+            this.lblMulta.AutoSize = true;
+            this.lblMulta.Location = new System.Drawing.Point(95, 212);
+            this.lblMulta.Name = "lblMulta";
+            this.lblMulta.Size = new System.Drawing.Size(0, 13);
+            this.lblMulta.TabIndex = 5;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(145, 140);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 6;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(739, 584);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.dgvLista);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Libritos Mark";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLista)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNombreLibro;
+        private System.Windows.Forms.TextBox txtAutor;
+        private System.Windows.Forms.TextBox txtCantidadHojas;
+        private System.Windows.Forms.TextBox txtPrecioRenta;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cmbxClasificacion;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DateTimePicker DTPFechaLibro;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView dgvLista;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NombreLibro;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Autor;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Clasificacion;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Cantidad;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Fecha;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Precio;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DateTimePicker DTPFechaProgramada;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label lblMulta;
+        private System.Windows.Forms.DateTimePicker DTPEntregaReal;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
